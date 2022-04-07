c0df3185918f41c41159eee5670983e7ed7f672b
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-04-07 12:19:05 +0530

    Created AddressBook Contacts

---
+++ AddressBook.js
@@ -0,0 +1,11 @@
+var prompt = require("prompt-sync")();
+
+//Taking an User Input 
+var FirstName = prompt("Enter the First Name :");
+var lastName = prompt("Enter the Last Name :");
+var address = prompt("Enter the address :");
+var city = prompt("Enter the city Name:");
+var state = prompt("Enter the State Name :");
+var zip = prompt("Enter the Zip code:");
+var phoneNo = prompt("Enter the Phone Number :");
+var email = prompt("Enter the Email :");
